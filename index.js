6848b16d509c9ab4a16affacb77d0c1b87ea5d55
Barkat ullah <barkatullah585464@gmail.com>
2023-10-21 11:18:10 +0600

    make client collection

--- index.js
+++ index.js
@@ -22,30 +22,48 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const foodCollection = client.db("foodsDB").collection("foods");
+    const madhangsikCollection = client.db("foodsDB").collection("madhangsik");
+    const dhangsikCollection = client.db("foodsDB").collection("dhangsik");
+    const redoneCollection = client.db("foodsDB").collection("redone");
+    const pepsicoCollection = client.db("foodsDB").collection("pepsico");
+    const mustafaCollection = client.db("foodsDB").collection("mustafa");
+    const cocacolaCollection = client.db("foodsDB").collection("cocacola");
 
-    app.get("/foods", async (req, res) => {
-      const result = await foodCollection.find().toArray();
+    const clientCollection = client.db("foodsDB").collection("client");
+
+
+    app.get("/client", async (req, res) => {
+      const result = await clientCollection.find().toArray();
       res.send(result);
     });
     
-// supplier_brand
 
-    // app.get("/foods/:supplier_brand", async (req, res) => {
-    //     const id = req.params.supplier_brand;
-    //     const query = { supplier: new ObjectId(id) };
-    //     const result = await foodCollection.findOne(query);
-    //     res.send(result);
-    //   });
-
-    app.get("/foods/:id", async (req, res) => {
+    app.get("/client/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      const result = await foodCollection.findOne(query);
+      const result = await clientCollection.findOne(query);
       res.send(result);
     });
+
+    app.post("/client", async (req, res) => {
+        const food = req.body;
+        const result = await clientCollection.insertOne(food);
+        console.log(result);
+        res.send(result);
+      });
+
+    app.delete("/client/:id", async (req, res) => {
+        const id = req.params.id;
+        console.log(id);
+        const query = { _id: new ObjectId(id) };
+        console.log(query);
+        const result = await clientCollection.deleteOne(query);
+        res.send(result);
+      });
+  
 
     app.put("/foods/:id", async (req, res) => {
       const id = req.params.id;
@@ -88,8 +106,278 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/madhangsik", async (req, res) => {
+        const result = await madhangsikCollection.find().toArray();
+        res.send(result);
+      });
+
+      app.get("/madhangsik/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await madhangsikCollection.findOne(query);
+        res.send(result);
+      });
+
+      app.post("/madhangsik", async (req, res) => {
+        const food = req.body;
+        const result = await madhangsikCollection.insertOne(food);
+        console.log(result);
+        res.send(result);
+      });
+
+      
+      app.put("/madhangsik/:id", async (req, res) => {
+        const id = req.params.id;
+        const updatedFood = req.body;
+       
+        const filter = { _id: new ObjectId(id) };
+        const options = { upsert: true };
+        const food = {
+          $set: {
+            name: updatedFood.name,
+            quantity: updatedFood.quantity,
+            supplier: updatedFood.supplier,
+            taste: updatedFood.taste,
+            category: updatedFood.category,
+            details: updatedFood.details,
+            photo: updatedFood.photo,
+          },
+        };
+        const result = await madhangsikCollection.updateOne(
+          filter,
+          food,
+          options
+        );
+        res.send(result);
+      });
+
+    app.get("/dhangsik", async (req, res) => {
+        const result = await dhangsikCollection.find().toArray();
+        res.send(result);
+      });
+
+      app.get("/dhangsik/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await dhangsikCollection.findOne(query);
+        res.send(result);
+      });
+
+      app.post("/dhangsik", async (req, res) => {
+        const food = req.body;
+        const result = await dhangsikCollection.insertOne(food);
+        console.log(result);
+        res.send(result);
+      });
+
+      app.put("/dhangsik/:id", async (req, res) => {
+        const id = req.params.id;
+        const updatedFood = req.body;
+       
+        const filter = { _id: new ObjectId(id) };
+        const options = { upsert: true };
+        const food = {
+          $set: {
+            name: updatedFood.name,
+            quantity: updatedFood.quantity,
+            supplier: updatedFood.supplier,
+            taste: updatedFood.taste,
+            category: updatedFood.category,
+            details: updatedFood.details,
+            photo: updatedFood.photo,
+          },
+        };
+        const result = await dhangsikCollection.updateOne(
+          filter,
+          food,
+          options
+        );
+        res.send(result);
+      });
+
+    app.get("/redone", async (req, res) => {
+        const result = await redoneCollection.find().toArray();
+        res.send(result);
+      });
+
+      app.get("/redone/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await redoneCollection.findOne(query);
+        res.send(result);
+      });
+
+      app.post("/redone", async (req, res) => {
+        const food = req.body;
+        const result = await redoneCollection.insertOne(food);
+        console.log(result);
+        res.send(result);
+      });
+
+      app.put("/redone/:id", async (req, res) => {
+        const id = req.params.id;
+        const updatedFood = req.body;
+       
+        const filter = { _id: new ObjectId(id) };
+        const options = { upsert: true };
+        const food = {
+          $set: {
+            name: updatedFood.name,
+            quantity: updatedFood.quantity,
+            supplier: updatedFood.supplier,
+            taste: updatedFood.taste,
+            category: updatedFood.category,
+            details: updatedFood.details,
+            photo: updatedFood.photo,
+          },
+        };
+        const result = await redoneCollection.updateOne(
+          filter,
+          food,
+          options
+        );
+        res.send(result);
+      });
+
+    app.get("/pepsico", async (req, res) => {
+        const result = await pepsicoCollection.find().toArray();
+        res.send(result);
+      });
+
+      app.get("/pepsico/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await pepsicoCollection.findOne(query);
+        res.send(result);
+      });
+
+      app.post("/pepsico", async (req, res) => {
+        const food = req.body;
+        const result = await pepsicoCollection.insertOne(food);
+        console.log(result);
+        res.send(result);
+      });
+
+           
+      app.put("/pepsico/:id", async (req, res) => {
+        const id = req.params.id;
+        const updatedFood = req.body;
+       
+        const filter = { _id: new ObjectId(id) };
+        const options = { upsert: true };
+        const food = {
+          $set: {
+            name: updatedFood.name,
+            quantity: updatedFood.quantity,
+            supplier: updatedFood.supplier,
+            taste: updatedFood.taste,
+            category: updatedFood.category,
+            details: updatedFood.details,
+            photo: updatedFood.photo,
+          },
+        };
+        const result = await pepsicoCollection.updateOne(
+          filter,
+          food,
+          options
+        );
+        res.send(result);
+      });
+
+    app.get("/mustafa", async (req, res) => {
+        const result = await mustafaCollection.find().toArray();
+        res.send(result);
+      });
+
+      app.get("/mustafa/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await mustafaCollection.findOne(query);
+        res.send(result);
+      });
+
+      app.post("/mustafa", async (req, res) => {
+        const food = req.body;
+        const result = await mustafaCollection.insertOne(food);
+        console.log(result);
+        res.send(result);
+      });
+      
+      app.put("/mustafa/:id", async (req, res) => {
+        const id = req.params.id;
+        const updatedFood = req.body;
+       
+        const filter = { _id: new ObjectId(id) };
+        const options = { upsert: true };
+        const food = {
+          $set: {
+            name: updatedFood.name,
+            quantity: updatedFood.quantity,
+            supplier: updatedFood.supplier,
+            taste: updatedFood.taste,
+            category: updatedFood.category,
+            details: updatedFood.details,
+            photo: updatedFood.photo,
+          },
+        };
+        const result = await mustafaCollection.updateOne(
+          filter,
+          food,
+          options
+        );
+        res.send(result);
+      });
+
+    app.get("/cocacola", async (req, res) => {
+        const result = await cocacolaCollection.find().toArray();
+        res.send(result);
+      });
+
+      app.get("/cocacola/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await cocacolaCollection.findOne(query);
+        res.send(result);
+      });
+
+      app.post("/cocacola", async (req, res) => {
+        const food = req.body;
+        const result = await cocacolaCollection.insertOne(food);
+        console.log(result);
+        res.send(result);
+      });
+
+      app.put("/cocacola/:id", async (req, res) => {
+        const id = req.params.id;
+        const updatedFood = req.body;
+       
+        const filter = { _id: new ObjectId(id) };
+        const options = { upsert: true };
+        const food = {
+          $set: {
+            name: updatedFood.name,
+            quantity: updatedFood.quantity,
+            supplier: updatedFood.supplier,
+            taste: updatedFood.taste,
+            category: updatedFood.category,
+            details: updatedFood.details,
+            photo: updatedFood.photo,
+          },
+        };
+        const result = await cocacolaCollection.updateOne(
+          filter,
+          food,
+          options
+        );
+        res.send(result);
+      });
+
+
+
+
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
